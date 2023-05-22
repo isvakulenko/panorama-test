aec33579d2f21f02ffda2c1cc4a53f716f363ed0
isvakulenko <ivan-vakulenko@yandex.ru>
2023-05-22 06:52:31 +0300

    Add component for select features

---
+++ src/components/GISDataSelect.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-container>
+    <v-row class="text-center">
+  <v-select
+    v-model="selectedFeature"
+    :items="features"
+    item-text="properties.name"
+    item-value="id"
+    label="Выбор feature"
+  ></v-select>
+</v-row>
+  </v-container>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data: () => ({
+    selectedFeature: null,
+  }),
+  props: {
+    features: {
+      type: Array,
+      default: [],
+    },
+  },
+  methods: {
+    selectFeature() {
+    
+       console.log('selectFeature', this.selectedFeature)
+      return this.selectedFeature;
+    },
+  },
+});
+</script>
+
+
