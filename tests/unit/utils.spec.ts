d2c993f3fa50f58898c627398bcc3f85e123c204
isvakulenko <ivan-vakulenko@yandex.ru>
2023-05-24 06:41:54 +0300

    Add unit tests for area and random number

---
+++ tests/unit/utils.spec.ts
@@ -0,0 +1,15 @@
+import { Utils } from '@/utils/utils';
+
+describe('Utils', () => {
+  it('calculates area correctly', () => {
+    const bbox = [0, 0, 5, 5];
+    const area = Utils.calculateArea(bbox);
+    expect(area).toBe('25.00');
+  });
+
+  it('generates random number between 7 and 10', () => {
+    const delay = Utils.getRandomNumber();
+    expect(delay).toBeGreaterThanOrEqual(7000);
+    expect(delay).toBeLessThanOrEqual(10000);
+  });
+});
