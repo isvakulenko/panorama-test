c535123061d63980c52c85f0798db924f5350e24
Ivan Vakulenko <65488123+isvakulenko@users.noreply.github.com>
2023-05-24 10:28:59 +0300

    Update const.ts
    eslint-disable

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export const baseUrl: string =
   'https://gisserver.info/GISWebServiceSE/service.php?RESTMETHOD=GETFEATURE&LAYER=noginsk_area_3d&SEMANTICNAME=1&MAPID=1&IDLIST=%D0%9D%D0%BE%D0%B3%D0%B8%D0%BD%D1%81%D0%BA.146736,%D0%9D%D0%BE%D0%B3%D0%B8%D0%BD%D1%81%D0%BA.24640&OUTTYPE=JSON';
 
