764b5950d9efbd1dc6e38e95954bdabd4dad6b89
isvakulenko <ivan-vakulenko@yandex.ru>
2023-05-26 06:34:24 +0300

    Refactor for class component

---
+++ src/App.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import {Component, Vue} from "vue-property-decorator";
+import GISGetData from "./components/GISGetData.vue";
+
+
+@Component({components: {GISGetData}})
+export default class App extends Vue {
+}
--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-row class="text-center">
+      <v-row class="text-center pl-15">
       <h2 class="headline font-weight-bold mb-3" >GIS Data</h2>
       </v-row>
     </v-app-bar>
@@ -11,20 +11,4 @@
     </v-main>
   </v-app>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-import GISGetData from "./components/GISGetData.vue";
-
-export default Vue.extend({
-  name: "App",
-
-  components: {
-    GISGetData
-},
-
-  data: () => ({
-    //
-  }),
-});
-</script>
+<script lang="ts" src="./App.ts"></script>
